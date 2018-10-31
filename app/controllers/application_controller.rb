3175dda20719e05671c808073db13f70b49b13e8
ViktoriaB2018 <bvo2008@mail.ru>
2018-10-31 21:15:31 +0200

    Permit username for sing up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+	before_action :сonfigure_permitted_parameters, if: :devise_controller?
+
+	private
+
+	def сonfigure_permitted_parameters
+		devise_parameter_sanitizer.for(:sign_up) << :username
+	end
 end
