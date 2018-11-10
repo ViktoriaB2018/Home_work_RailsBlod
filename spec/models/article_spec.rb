d7bf64c7f97ba0548129ecc80328f0ab299a4fe0
ViktoriaB2018 <bvo2008@mail.ru>
2018-11-10 19:56:51 +0200

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,5 @@
 require "rails_helper"
+require 'support/factory_girl'
 
 describe Article do
 	describe "validations" do
@@ -8,5 +9,15 @@ describe Article do
 
 	describe "assosiations" do
 		it { should have_many :comments }
+	end
+
+	describe "#subject" do
+		it "returns the article title" do
+			# создаем обїект article другим способом, не active record - arrange, act
+			article = create(:article, title: 'Lorem Ipsum')
+
+			# assert, проверка
+			expect(article.subject).to eq 'Lorem Ipsum'
+		end
 	end
 end
