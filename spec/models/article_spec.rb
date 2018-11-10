9cf09c90e11ed34487f31a2adf55a03a5e193ba3
ViktoriaB2018 <bvo2008@mail.ru>
2018-11-10 17:27:42 +0200

    Change specs on simple way

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
-RSpec.describe Article, type: :model do
+describe Article do
 	describe "validations" do
-		it { should validate_presence_of(:title) }
-		it { should validate_presence_of(:text) }
+		it { should validate_presence_of :title }
+		it { should validate_presence_of :text }
 	end
 
 	describe "assosiations" do
-		it { should have_many(:comments) }
+		it { should have_many :comments }
 	end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
-RSpec.describe Comment, type: :model do
-	it { should belong_to(:article) }
+describe Comment do
+	it { should belong_to :article }
 end
