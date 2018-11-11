4579e6353bcfec25ca0a8b5e36aec27d81769973
ViktoriaB2018 <bvo2008@mail.ru>
2018-11-11 20:28:30 +0200

    Update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,17 @@ FactoryGirl.define do
   factory :article do
   	title "Article title"
   	text "Article text"
-  end
+
+  # создаем фабрику с названием article_with_comments для создания статьи с несколькими комментариями
+	  factory :article_with_comments do
+	  	transient do
+	        comments_count 3
+	    end
+	  	# после создания article
+	  	after :create do |article, evaluator|
+	  			# создаем список из 3-х комментариев
+	        create_list :comment, evaluator.comments_count, article: article
+	    end
+	  end
+	end
 end
