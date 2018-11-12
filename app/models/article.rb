515744db456471d2ac4c1b8cbad6c637172a6185
ViktoriaB2018 <bvo2008@mail.ru>
2018-11-12 19:15:32 +0200

    Update article validation for title and text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-	validates :title, presence: true
-	validates :text, presence: true
+	validates :title, presence: true, length: { in: 6..100 }
+	validates :text, presence: true, length: { in: 10..4000 }
 	has_many :comments
 
 	def subject
