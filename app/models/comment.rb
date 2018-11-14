0a92b4b7020481c95044125fbc140010f59958eb
ViktoriaB2018 <bvo2008@mail.ru>
2018-11-14 17:39:57 +0200

    Update comment validation and add length spec

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
-	validates :body, presence: true, length: { in: 5..4000 }
+	validates :body, presence: true
+	validates_length_of :body, in: 5..4000
+	
   belongs_to :article
 end
