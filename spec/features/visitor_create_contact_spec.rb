cefc83143bfd64333cf04833c2d649546f68a25b
ViktoriaB2018 <bvo2008@mail.ru>
2018-11-15 17:34:09 +0200

    Update contacts view

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 feature "Contact Creation" do
 	scenario "allows acces to contacts page" do
 		visit '/contacts'
-		expect(page).to have_content 'Contact us'
+		expect(page).to have_content 'Contact Us'
 	end
 end
